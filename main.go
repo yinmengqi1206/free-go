ae5c33dfd3f2697c500648e76868522b640df9ae
yinmengqi <13121552460@163.com>
2024-04-11 16:15:35 +0800

    go

--- main.go
+++ main.go
@@ -2,74 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
 )
 
-// go语言fmt.Printf格式输出
-//https://juejin.cn/post/7127305094273433613
-
-func main1() {
-	//multiplication()
-	// var ids = []int{1, 9, 2, 4, 3, 7} //定义一个数组有5个int元素
-	// count := bubbleSort(ids)
-	// fmt.Println(count)
-	//fmt.Println(expRecur(3))
-	for i := 0; i < 10; i++ {
-		for j := 'A'; j < 'F'; j++ {
-			if j == 'B' {
-				goto OVER
-			}
-			fmt.Printf("%v_%c \n", i, j)
-		}
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       // 解析参数，默认是不会解析的
+	fmt.Println(r.Form) // 这些信息是输出到服务器端的打印信息
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
 	}
-OVER:
-	fmt.Println("over")
+	fmt.Fprintf(w, "Hello astaxie!") // 这个写入到 w 的是输出到客户端的
 }
 
-// 99乘法表
-func multiplication() {
-	for i := 1; i < 10; i++ {
-		for j := 1; j <= i; j++ {
-			fmt.Printf("%d * %d = %d |", i, j, j*i)
-		}
-		fmt.Println()
+func main() {
+	http.HandleFunc("/", sayhelloName)       // 设置访问的路由
+	err := http.ListenAndServe(":9090", nil) // 设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
-}
-
-/* 平方阶（冒泡排序） */
-func bubbleSort(nums []int) int {
-	count := 0 // 计数器
-	// 外循环：待排序元素数量为 n-1, n-2, ..., 1
-	for i := len(nums) - 1; i > 0; i-- {
-		// 内循环：冒泡操作
-		for j := 0; j < i; j++ {
-			if nums[j] > nums[j+1] {
-				// 交换 nums[j] 与 nums[j + 1]
-				tmp := nums[j]
-				nums[j] = nums[j+1]
-				nums[j+1] = tmp
-				count += 3 // 元素交换包含 3 个单元操作
-			}
-		}
-	}
-	return count
-}
-
-// 指数阶
-func exponential(n int) int {
-	count, base := 0, 1
-	for i := 0; i < n; i++ {
-		for j := 0; j < base; j++ {
-			count++
-		}
-		base *= 2
-	}
-	return count
-}
-
-/* 指数阶（递归实现）*/
-func expRecur(n int) int {
-	if n == 1 {
-		return 1
-	}
-	return expRecur(n-1) + expRecur(n-1) + 1
 }
